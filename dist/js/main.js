b4b73bc78f56ca9f7b69224b561052ab80fa9e1e
kopki <kopki684@gmail.com>
2021-07-19 15:59:26 +0800

    portfolio scss

---
+++ dist/js/main.js
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector('.menu-btn')
+const hamburger = document.querySelector('.menu-btn__burger')
+const nav = document.querySelector('.nav')
+const menuNav = document.querySelector('.menu-nav')
+const navItems = document.querySelectorAll('.menu-nav__item')
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    hamburger.classList.toggle('open')
+    nav.classList.toggle('open')
+    menuNav.classList.toggle('open')
+    navItems.forEach(item => {
+        item.classList.toggle('open')
+    })
+}
